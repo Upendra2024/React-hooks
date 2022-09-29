55de7c5e252d7949018e8691760215f9d4ef68f5
Upendra2024 <92069507+Upendra2024@users.noreply.github.com>
2022-09-29 15:38:43 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./style.css";
 import {StateOne} from './usestate/StateOne';
+import {StateTwo} from './usestate/StateTwo';
 export default function App() {
   return (
     <div>
       <h1>UseState Exmples</h1>
       <p>here we take previous state  of properties(as a spred operator not need in class comp) while updatiiong state properties</p>
-      <StateOne/>
+      {/* <StateOne/> */}
+      <StateTwo/>
     </div>
   );
 }
---
+++ src/usestate/StateTwo.js
@@ -0,0 +1,29 @@
+import React,{useState} from 'react';
+
+export const StateTWo = ()=> {
+  const [state,setState] = useState([]);
+
+ const clickHandler =() =>{
+    setState([...state,
+      {
+        id:state.length,
+        value : Math.floor(Math.random * 10) + 1
+      
+       } ])
+  };
+
+  return(
+    <div>
+      <button onClick={clickHandler}>Add</button>
+      <ul>
+        {
+          state.map(item =>{
+            <li key = {item.id}>{item.value}</li>
+          })
+        }
+
+      </ul>
+
+    </div>
+  )
+}
