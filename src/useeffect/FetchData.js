f8b5061ca4e264e6794e3e29dbd05af51908a2f7
Upendra2024 <92069507+Upendra2024@users.noreply.github.com>
2022-10-03 16:31:49 +0530

    1 file modified

--- src/useeffect/FetchData.js
+++ src/useeffect/FetchData.js
@@ -2,25 +2,45 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export const FetchData = () => {
-  const [posts, setPosts] = useState([]);
+  //fetch all data in the endpoint
+  // const [posts, setPosts] = useState([]);
+
+  //fetching individul data in the endpoint through controlled component
+  const [post, setPost] = useState({});
+  const [id, setId] = useState();
+
+  // useEffect(() => {
+  //   axios
+  //     .get('https://jsonplaceholder.typicode.com/posts')
+  //     .then((res) => {
+  //       console.log(res);
+  //       setPosts(res.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
+
   useEffect(() => {
     axios
-      .get('https://jsonplaceholder.typicode.com/posts')
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then((res) => {
-        console.log(res);
-        setPosts(res.data);
+        setPost(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }, [id]);
+
   return (
     <div>
-      <ul>
+      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <div>{post.title}</div>
+      {/* <ul>
         {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 };
